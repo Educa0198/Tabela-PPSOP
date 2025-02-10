bf7e4f24ec6b9456398ed831517244549b17e6d5
Educa0198 <vieiraeduardo2006@gmail.com>
2025-02-10 19:58:39 -0300

    att

--- main.cpp
+++ main.cpp
@@ -1,79 +1,110 @@
 #include <iostream>
 #include <string>
 
-
 using namespace std;
 
-class jogador
-{
-
+class jogador {
     string nome;
     int pontos;
-    int posicao;
 
-    public:
-
-    jogador()
-    {
+public:
+    jogador() {
         setnome("Sem_Nome");
         setpontos(0);
-        setposicao(0);
-
+   
     }
-    jogador(string nome1, int pontos1, int posicao1)
-    {
+
+    jogador(string nome1, int pontos1) {
         setnome(nome1);
         setpontos(pontos1);
-        setposicao(posicao1);
+    
     }
 
-    void setnome(string nome1)
-    {
+    void setnome(string nome1) {
         nome = nome1;
     }
 
-    void setpontos( int pontos1)
-    {
+    void setpontos(int pontos1) {
         pontos = pontos1;
     }
 
-    void setposicao(int posicao1)
-    {
-        posicao = posicao1;
-    }
+   
 
-    string getnome()
-    {
+    string getnome() {
         return nome;
     }
 
-    int getpontos()
-    {
+    int getpontos() {
         return pontos;
     }
 
-    int getposicao()
-    {
-        return posicao; 
-    }
 };
 
-class tabela
-{
+class tabela {
     jogador** player;
     int numjogadores;
+public:
+    tabela(int numjogadores1) {
+        numjogadores = numjogadores1;
+        int posicao[numjogadores];
+        player = new jogador*[numjogadores];  // Alocação dinâmica do array de jogadores
+        for (int i = 0; i < numjogadores; i++) {
+            player[i] = new jogador();  // Criando jogadores padrão
+        }
+    }
 
-    public:
-    
-    void setnumjogadores( int numjogadores1)
-    {
+   
+
+    ~tabela() {  
+        // Destrutor para liberar a memória alocada dinamicamente
+        for (int i = 0; i < numjogadores; i++) {
+            delete player[i];
+        }
+        delete[] player;
+    }
+
+    void setnumjogadores(int numjogadores1) {
         numjogadores = numjogadores1;
     }
 
+    int getnumjogadores() {
+        return numjogadores;
+    }
+
+
+    void adicionarJogador(string nome, int pontos, int posicao) {
+        // Criar um novo array com tamanho maior
+        jogador** novoArray = new jogador*[numjogadores + 1];
+
+        // Copiar os jogadores existentes
+        for (int i = 0; i < numjogadores; i++) {
+            novoArray[i] = player[i];
+        }
+
+        // Criar e adicionar o novo jogador na última posição
+        novoArray[numjogadores] = new jogador(nome, pontos, posicao);
+
+        // Liberar o array antigo
+        delete[] player;
+
+        // Atualizar o ponteiro e o número de jogadores
+        player = novoArray;
+        numjogadores++;
+    }
+
+    void exibe() {
+        cout << "Lista de jogadores:\n";
+        for (int i = 0; i < numjogadores; i++) {
+            cout << "Nome: " << player[i]->getnome()
+                 << " | Pontos: " << player[i]->getpontos()
+                 << endl;
+    }
 };
 
-
-int main()
-{
-
+int main() {
+    tabela t(3);  // Criando uma tabela com 3 jogadores
+    t.exibe();  // Exibindo os jogadores
+    return 0;
 }
+
+
